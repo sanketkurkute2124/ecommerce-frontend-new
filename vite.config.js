931cbfe5085778c25e7418353874162f9b541d65
sanketkurkute2124 <sanketkurkute2124@gmail.com>
2026-06-08 19:02:28 +0530

    fixed the code for the reload

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,31 @@
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+// import path from "path";
+
+// export default defineConfig({
+//   plugins: [react()],
+//   resolve: {
+//     alias: {
+//       "@": path.resolve(__dirname, "./src"),
+//     },
+//   },
+// });
+
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
 
 export default defineConfig({
   plugins: [react()],
+
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      "@": path.resolve(process.cwd(), "src"),
     },
+  },
+
+  server: {
+    port: 5173,
+    open: true,
   },
 });
